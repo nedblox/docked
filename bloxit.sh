49e9156666ac1c9c82f0528290f8497f6956d733
nedblox <50992622+nedblox@users.noreply.github.com>
2019-05-24 21:46:54 +0530

    Update bloxit.sh

--- bloxit.sh
+++ bloxit.sh
@@ -7,5 +7,5 @@ usermod -aG docker your-user
 mkdir BloxOne
 cd BloxOne
 wget http://ib-noa-prod.csp.infoblox.com.s3-website-us-east-1.amazonaws.com/BloxOne_OnPrem_Docker_2.0.11.tar.gz
-docker load i BloxOne_OnPrem_Docker_2.0.11.tar.gz
+docker load -i BloxOne_OnPrem_Docker_2.0.11.tar.gz
 sudo docker run -d --name blox.noa --restart=always --net=host -v /var/run/docker.sock:/var/run/docker.sock infobloxcto/onprem.agent:v2.0.11 --access-key=$KEY
